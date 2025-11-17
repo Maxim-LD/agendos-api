9bb91e66f71880652996a766be4b375f31cdf40e
maxim-ld <ayinde779@gmail.com>
2025-11-17 23:13:04 +0100

    refactor: restructure logger configuration for improved readability and maintainability

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,27 +1,32 @@
 import { createLogger, format, transports } from "winston";
 import { config } from "../config";
 
+const { combine, timestamp, json, colorize, simple, errors } = format;
+
+// Define transports based on the environment
+const loggerTransports = [];
+
+// In production, log to files and the console with JSON format
+if (config.nodeEnv === 'production') {
+    // loggerTransports.push(new transports.File({ filename: 'logs/error.log', level: 'error' }));
+    // loggerTransports.push(new transports.File({ filename: 'logs/combined.log' }));
+    loggerTransports.push(new transports.Console()); // Console transport for production
+} else {
+    // In development, use a more readable format for the console
+    loggerTransports.push(new transports.Console({
+        format: combine(
+            colorize(),
+            simple()
+        )
+    }));
+}
+
 export const logger = createLogger({
     level: config.nodeEnv === 'production' ? 'info' : 'debug',
-    format: format.combine(
-        format.errors({ stack: true }),
-        format.json()
+    format: combine(
+        // timestamp(),
+        errors({ stack: true }),
+        json()
     ),
-    // transports: [
-    //     new transports.File({ filename: 'logs/error.log', level: 'error' }),
-    //     new transports.File({ filename: 'logs/combined.log' })
-    // ]
-})
-
-// if (config.nodeEnv === 'production') {
-
-// }
-
-if (config.nodeEnv === 'production') {
-    logger.add(new transports.Console({
-        format: format.combine(
-            format.colorize(),
-            format.simple()
-        )
-    }))
-}
+    transports: loggerTransports
+});
