f9364d9826706fe750f298a59cffa2efe34dec30
maxim-ld <ayinde779@gmail.com>
2025-11-17 18:15:19 +0100

    feat: add secure option to SMTP configuration and improve email transporter initialization

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,18 +1,23 @@
 import { createLogger, format, transports } from "winston";
+import { config } from "../config";
 
 export const logger = createLogger({
-    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+    level: config.nodeEnv === 'production' ? 'info' : 'debug',
     format: format.combine(
         format.errors({ stack: true }),
         format.json()
     ),
-    transports: [
-        new transports.File({ filename: 'logs/error.log', level: 'error' }),
-        new transports.File({ filename: 'logs/combined.log' })
-    ]
+    // transports: [
+    //     new transports.File({ filename: 'logs/error.log', level: 'error' }),
+    //     new transports.File({ filename: 'logs/combined.log' })
+    // ]
 })
 
-if (process.env.NODE_ENV !== 'production') {
+// if (config.nodeEnv === 'production') {
+
+// }
+
+if (config.nodeEnv === 'production') {
     logger.add(new transports.Console({
         format: format.combine(
             format.colorize(),
